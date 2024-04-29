74443fb41db4877d743b95efda89c47704c292b6
Cleyson <cleysonph@gmail.com>
2024-04-29 18:48:41 -0300

    implementa async

--- src/main/java/br/com/treinaweb/springasync/core/services/mail/FakeMailService.java
+++ src/main/java/br/com/treinaweb/springasync/core/services/mail/FakeMailService.java
@@ -1,6 +1,7 @@
 package br.com.treinaweb.springasync.core.services.mail;
 
 import org.springframework.stereotype.Service;
+import org.springframework.scheduling.annotation.Async;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -8,14 +9,15 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 public class FakeMailService implements MailService {
 
+    @Async
     @Override
     public void send(String to, String subject, String body) {
-        log.info("Enviando e-mail para {} com o assunto {}", to, subject);
         try {
             Thread.sleep(5000);
         } catch (InterruptedException e) {
             log.error("Erro ao enviar e-mail: {}", e.getMessage());
         }
+        log.info("E-mail enviando para {} com o assunto {}", to, subject);
     }
     
 }
